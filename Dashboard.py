b62c182733eaf54b8ad81b67261c6ea2897a5000
angie230705 <ay.leivag@uea.edu.ec>
2025-02-02 20:09:15 -0500

    Add files via upload

---
+++ Dashboard.py
@@ -0,0 +1,50 @@
+import os
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1':'ANGELINE-2',
+        '2': 'Clase, Definición de Objeto, Herencia, Encapsulación y Polimorfismo.py',
+        '3': 'EJEMPLOS MUNDO REAL POO.py',
+        '4': 'Implementación de Constructores y Destructores en Python.py',
+        '5': 'Tipos de datos, Identificadores. Implementación y Publicación de Código.py',
+
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+
+    while True:
+        print("\n********Menu Principal - Dashboard*************")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
